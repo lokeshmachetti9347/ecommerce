c8b15db33b3ee0014456286b29d5497f4c4e037d
lokeshmachetti9347 <lokeshmachetti9347@gmail.com>
2026-07-30 11:19:39 +0530

    added the database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base 
+DATABASE_URL = "mysql+pymysql://root:Lokesh%40123@localhost:3306/ecommerce"
+engine = create_engine(DATABASE_URL) 
+SessionLocal = sessionmaker( 
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine 
+    ) 
+Base = declarative_base()
