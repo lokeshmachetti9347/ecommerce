233348d9eebe491814b13b32eeff5046137cbb51
Lokesh Matchetti9347 <lokeshmachetti9347@gmail.com>
2026-07-30 14:36:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base 
 # DATABASE_URL = "mysql+pymysql://root:Lokesh%40123@localhost:3306/ecommerce"
-DATABASE_URL ="mysql+pymysql://CLICK_TO:REVEAL_PASSWORD@mysql-bd40758-lokeshmachetti9347-22f6.d.aivencloud.com:11771/defaultdb"
+# DATABASE_URL ="mysql+pymysql://CLICK_TO:REVEAL_PASSWORD@mysql-bd40758-lokeshmachetti9347-22f6.d.aivencloud.com:11771/defaultdb"
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_zbCupRnEDksbVXiw9un@mysql-bd40758-lokeshmachetti9347-22f6.d.aivencloud.com:11771/defaultdb"
 engine = create_engine(DATABASE_URL) 
 SessionLocal = sessionmaker( 
     autocommit=False, 
